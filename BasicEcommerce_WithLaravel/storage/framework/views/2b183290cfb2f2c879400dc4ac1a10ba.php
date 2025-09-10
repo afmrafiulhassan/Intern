daf6d35d8b42843bc33f679f08e261f0a1aef5be
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2025-09-10 10:45:29 +0600

    Add files via upload

---
+++ BasicEcommerce_WithLaravel/storage/framework/views/2b183290cfb2f2c879400dc4ac1a10ba.php
@@ -0,0 +1,9 @@
+<div class="card">
+    <div class="card-header"><?php echo e($title); ?></div>
+
+    <?php echo e($slot); ?>
+
+
+    <div class="card-footer"><?php echo e($footer); ?></div>
+
+</div><?php /**PATH C:\xampp\htdocs\Internship\BasicEcommerce_WithLaravel\resources\views/components/card.blade.php ENDPATH**/ ?>
---
+++ BasicEcommerce_WithLaravel/storage/framework/views/c45649c71cd32ccd5e6fa17c89d9eb68.php
@@ -0,0 +1,4 @@
+<button
+    class="btn btn-default flex justify-center items-center gap-1">
+    <img src="/img/google.png" alt="" style="width: 20px" />Google
+</button><?php /**PATH C:\xampp\htdocs\Internship\BasicEcommerce_WithLaravel\resources\views/components/google-button.blade.php ENDPATH**/ ?>
